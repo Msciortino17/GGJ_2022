4f01f439c16c69131f00dcede75f66637b82fc96
Monish Gupta <monish1478@gmail.com>
2022-01-30 01:29:33 -0500

    Added Death Effects for Heaven and Hell. Health Bar Color Mapping

--- Assets/Scripts/MiniGames/HeavenHell/Minion.cs
+++ Assets/Scripts/MiniGames/HeavenHell/Minion.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Minion : MonoBehaviour
 {
@@ -19,12 +21,16 @@ public class Minion : MonoBehaviour
 	public Transform MyHealthBar;
 	private float fullHealthScale;
 	private float fightShakeSpeed;
+	
+	private SpriteRenderer myHealthBarRenderer;
 
 	public AudioSource MyFightSound;
+	public GameObject DeathParticles;
 
 	// Start is called before the first frame update
 	void Start()
 	{
+		myHealthBarRenderer = MyHealthBar.GetComponent<SpriteRenderer>();
 		fullHealthScale = MyHealthBar.localScale.x;
 		FullHealth = Health;
 		fightShakeSpeed = Random.Range(5f, 10f);
@@ -78,6 +84,12 @@ public class Minion : MonoBehaviour
 		Vector3 scale = MyHealthBar.transform.localScale;
 		scale.x = ((float)Health / (float)FullHealth) * fullHealthScale;
 		MyHealthBar.transform.localScale = scale;
+
+		float H, S, V;
+		Color.RGBToHSV(myHealthBarRenderer.color, out H, out S, out V);
+
+		float mappedHue = map(scale.x, 0f, 1f, 0f, 0.33f);
+		myHealthBarRenderer.color = Color.HSVToRGB(mappedHue, S, V);
 	}
 
 	/// <summary>
@@ -97,6 +109,12 @@ public class Minion : MonoBehaviour
 	{
 		MyState = MinionState.Fighting;
 	}
+	
+	float map(float s, float a1, float a2, float b1, float b2)
+	{
+		return b1 + (s-a1)*(b2-b1)/(a2-a1);
+	}
+
 }
 
 public enum MinionState
